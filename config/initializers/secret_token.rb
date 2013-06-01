fafa5647e69b3476974fa9f6f6e3f9152eda7264
Frank W <info@wangzifeng.com>
2013-06-01 16:06:20 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBlackjack::Application.config.secret_token = '20162ecd520dabfd05597cd9438622c0c18a6c3d9e0fe9b9431fea29a053da427ef78a058ecbeed9964ba572cf5ce5be5922bbccc26f6a5d3094715651fca9e5'
